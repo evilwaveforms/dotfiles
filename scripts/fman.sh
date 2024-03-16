e552955b6b2a7e53777f287e1174e39c4da35969
evilwaveforms <8681846+evilwaveforms@users.noreply.github.com>
2024-03-16 14:59:06 +0200

    Add fman

---
+++ scripts/fman.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# fuzzy find man pages
+fman() {
+    while true; do
+        commands=$(apropos -s 1,8 . | awk '{print $1}')
+        selected=$(echo "$commands" | fzf --exact --sort --preview='man -P "cat" {} 2>/dev/null' --preview-window='right:60%' --prompt "Search commands: ")
+        [[ -z $selected ]] && return 0
+        man "$selected"
+    done
+}
