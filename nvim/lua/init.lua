b541211f5710833c1af04877c89883b51a086cc2
evilwaveforms <8681846+evilwaveforms@users.noreply.github.com>
2024-04-04 17:51:30 +0300

    Update lsp config

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -4,14 +4,14 @@ vim.g.syntax_on = false
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -22,32 +22,62 @@ require('fzf').setup()
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  client.server_capabilities.semanticTokensProvider = nil
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end)
-  vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end)
-  vim.keymap.set("n", "<leader>k", function() vim.lsp.buf.hover() end)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end)
-  vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end)
-  lsp_zero.default_keymaps({buffer = bufnr})
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    client.server_capabilities.semanticTokensProvider = nil
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end)
+    vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end)
+    vim.keymap.set("n", "<leader>k", function() vim.lsp.buf.hover() end)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end)
+    vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end)
+    lsp_zero.default_keymaps({buffer = bufnr})
 end)
+
+local on_attach = function(client, bufnr)
+    if client.name == 'ruff_lsp' then
+        -- Disable hover in favor of Pyright
+        client.server_capabilities.hoverProvider = false
+    end
+end
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {},
-  handlers = {
-    lsp_zero.default_setup,
-    lua_ls = function()
-      local lua_opts = lsp_zero.nvim_lua_ls()
-      require('lspconfig').lua_ls.setup(lua_opts)
-    end,
-  }
+    ensure_installed = { "basedpyright", "ruff_lsp", "rust_analyzer" },
+    handlers = {
+        lsp_zero.default_setup,
+        lua_ls = function()
+            local lua_opts = lsp_zero.nvim_lua_ls()
+            require('lspconfig').lua_ls.setup(lua_opts)
+        end,
+        basedpyright = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            require('lspconfig').basedpyright.setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+                settings = {
+                    basedpyright = {
+                        typeCheckingMode = "standard",
+                        disableOrganizeImports = true,
+                    },
+                    python = {
+                        analysis = {
+                            ignore = { '*' },
+                        }
+                    },
+                },
+            })
+        end,
+        ruff_lsp = function()
+            require("lspconfig").ruff_lsp.setup({
+                on_attach = on_attach,
+            })
+        end,
+    }
 })
 
 local cmp = require('cmp')
@@ -55,39 +85,39 @@ local cmp_action = require('lsp-zero').cmp_action()
 local cmp_format = lsp_zero.cmp_format()
 
 local function border(hl_name)
-  return {
-    { "┌", hl_name },
-    { "─", hl_name },
-    { "┐", hl_name },
-    { "│", hl_name },
-    { "┘", hl_name },
-    { "─", hl_name },
-    { "└", hl_name },
-    { "│", hl_name },
-  }
+    return {
+        { "┌", hl_name },
+        { "─", hl_name },
+        { "┐", hl_name },
+        { "│", hl_name },
+        { "┘", hl_name },
+        { "─", hl_name },
+        { "└", hl_name },
+        { "│", hl_name },
+    }
 end
 
 cmp.setup({
-  formatting = cmp_format,
-  mapping = cmp.mapping.preset.insert({
-    -- scroll up and down the documentation window
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
-    ['<Tab>'] = cmp_action.tab_complete(),
-    ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
-    ['<C-e>'] = cmp.mapping.abort(),
-  }),
-  window = {
-    completion = {
-      border = border("CmpMenuBorder"),
-      -- winhighlight = "Normal:CmpMenu,CursorLine:CmpMenuSel,Search:None",
-    },
-    documentation = {
-      border = border("CmpMenuBorder"),
-      -- winhighlight = "Normal:CmpDoc",
-    },
-  }
+    formatting = cmp_format,
+    mapping = cmp.mapping.preset.insert({
+        -- scroll up and down the documentation window
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        ['<CR>'] = cmp.mapping.confirm({select = false}),
+        ['<Tab>'] = cmp_action.tab_complete(),
+        ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+        ['<C-e>'] = cmp.mapping.abort(),
+    }),
+    window = {
+        completion = {
+            border = border("CmpMenuBorder"),
+            -- winhighlight = "Normal:CmpMenu,CursorLine:CmpMenuSel,Search:None",
+        },
+        documentation = {
+            border = border("CmpMenuBorder"),
+            -- winhighlight = "Normal:CmpDoc",
+        },
+    }
 })
 
 vim.diagnostic.config({
