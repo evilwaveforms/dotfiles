77d2873695e084ffe86500a44c0d6cf608f8bca9
evilwaveforms <8681846+evilwaveforms@users.noreply.github.com>
2024-05-09 13:44:33 +0300

    Make it zsh compatible

--- scripts/fman.sh
+++ scripts/fman.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # fuzzy find man pages
 fman() {
+    local commands selected
     while true; do
         commands=$(apropos -s 1,8 . | awk '{print $1}')
         selected=$(echo "$commands" | fzf --exact --sort --preview='man -P "cat" {} 2>/dev/null' --preview-window='right:60%' --prompt "Search commands: ")
-        [[ -z $selected ]] && return 0
+        [ -z "$selected" ] && return 0
         man "$selected"
     done
 }
