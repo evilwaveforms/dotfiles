af33df98f3337747f8ec9a9427408360996380a4
evilwaveforms <8681846+evilwaveforms@users.noreply.github.com>
2024-07-14 12:35:00 +0300

    Update note script

--- scripts/note.sh
+++ scripts/note.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 note() {
-    NOTES_DIR="$HOME/notes/d"
+    NOTES_DIR="$HOME/notes"
     CURRENT_DATE=$(date +'%d-%m-%Y')
-    FILE_PATH="$NOTES_DIR/$CURRENT_DATE.md"
-    mkdir -p "$NOTES_DIR"
+    if [ $# -eq 0 ]; then
+        FILE_PATH="$NOTES_DIR/d/$CURRENT_DATE.md"
+    else
+        NOTE_NAME="$1"
+        FILE_PATH="$NOTES_DIR/${NOTE_NAME}.md"
+    fi
+    mkdir -p "$NOTES_DIR/d"
     if [ ! -f "$FILE_PATH" ]; then
         touch "$FILE_PATH"
     fi
