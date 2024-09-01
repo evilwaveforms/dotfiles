573a918c5bb6d30abf488a6f5bfbc89c12a79ddb
evilwaveforms <8681846+evilwaveforms@users.noreply.github.com>
2024-09-01 15:36:30 +0300

    Add blinking cursor

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -256,6 +256,12 @@ vim.opt.incsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
+vim.o.guicursor = table.concat({
+  "n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+  "i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+  "r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100"
+}, ",")
+
 vim.g.go_fmt_autosave = 1
 vim.g.go_gopls_enabled = 1
 vim.g.go_highlight_types = 1
